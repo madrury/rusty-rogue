a43eb6c4d8d3e119e8332a4192950bd5ed5cb5b9
Matthew Drury <matthew.drury.83@gmail.com>
2021-02-26 00:23:56 -0800

    Add tool tips.

--- rogue/src/gui.rs
+++ rogue/src/gui.rs
@@ -1,7 +1,6 @@
-use rltk::{ RGB, Rltk, Console };
+use super::{CombatStats, GameLog, Map, Name, Player, Position};
+use rltk::{Console, Point, Rltk, RGB};
 use specs::prelude::*;
-use super::{CombatStats, Player, GameLog};
-
 
 const XPOSITION: i32 = 0;
 const YPOSITION: i32 = 43;
@@ -16,15 +15,20 @@ const HEALTH_BAR_WIDTH: i32 = 51;
 
 const N_LOG_LINES: i32 = 5;
 
-
 pub fn draw_ui(ecs: &World, ctx: &mut Rltk) {
+    // Draw the outline for the gui: A rectangle at the bottom of the console.
+    ctx.draw_box(
+        XPOSITION,
+        YPOSITION,
+        BOXWIDTH,
+        BOXHEIGHT,
+        RGB::named(rltk::WHITE),
+        RGB::named(rltk::BLACK),
+    );
 
-    // A rectangle at the bottom of the console.
-    ctx.draw_box(XPOSITION,  YPOSITION, BOXWIDTH, BOXHEIGHT, RGB::named(rltk::WHITE), RGB::named(rltk::BLACK));
-
+    // Draw the players healthbar at the top of the gui.
     let combat_stats = ecs.read_storage::<CombatStats>();
     let players = ecs.read_storage::<Player>();
-
     for (_player, stats) in (&players, &combat_stats).join() {
         let health = format!(" HP: {} / {} ", stats.hp, stats.max_hp);
         ctx.print_color(
@@ -32,7 +36,7 @@ pub fn draw_ui(ecs: &World, ctx: &mut Rltk) {
             HEALTH_TEXT_YPOSITION,
             RGB::named(rltk::YELLOW),
             RGB::named(rltk::BLACK),
-            &health
+            &health,
         );
         ctx.draw_bar_horizontal(
             HEALTH_BAR_XPOSITION,
@@ -41,14 +45,116 @@ pub fn draw_ui(ecs: &World, ctx: &mut Rltk) {
             stats.hp,
             stats.max_hp,
             RGB::named(rltk::RED),
-            RGB::named(rltk::BLACK)
+            RGB::named(rltk::BLACK),
         );
     }
 
+    // Draw the gamelog inside the gui.
     let log = ecs.fetch::<GameLog>();
     for (i, s) in log.entries.iter().rev().enumerate() {
         if i < N_LOG_LINES as usize {
             ctx.print(2, YPOSITION as usize + i + 1, s)
+        }
+    }
+
+    // Render the mouse position.
+    let mpos = ctx.mouse_pos();
+    ctx.set_bg(mpos.0, mpos.1, RGB::named(rltk::MAGENTA));
+
+    draw_tooltips(ecs, ctx);
+}
+
+fn draw_tooltips(ecs: &World, ctx: &mut Rltk) {
+    let map = ecs.fetch::<Map>();
+    let names = ecs.read_storage::<Name>();
+    let positions = ecs.read_storage::<Position>();
+
+    let mpos = ctx.mouse_pos();
+    if mpos.0 >= map.width || mpos.1 >= map.height {
+        return;
+    }
+
+    // Grab the names of the entities at the mouse position,
+    let mut tooltip: Vec<String> = Vec::new();
+    for (name, pos) in (&names, &positions).join() {
+        let idx = map.xy_idx(pos.x, pos.y);
+        if pos.x == mpos.0 && pos.y == mpos.1 && map.visible_tiles[idx] {
+            tooltip.push(name.name.to_string());
+        }
+    }
+
+    // Draw the tooltip.
+    if !tooltip.is_empty() {
+        let mut width: i32 = 0;
+        for s in tooltip.iter() {
+            if width < s.len() as i32 {
+                width = s.len() as i32;
+            }
+        }
+        width += 2;
+
+        if mpos.0 > map.width / 2 {
+            let arrow_pos = Point::new(mpos.0 - 1, mpos.1);
+            let left_x = mpos.0 - width;
+            let mut y = mpos.1;
+            for s in tooltip.iter() {
+                ctx.print_color(
+                    left_x,
+                    y,
+                    RGB::named(rltk::WHITE),
+                    RGB::named(rltk::GREY),
+                    s,
+                );
+                let padding = (width - s.len() as i32);
+                for i in 0..padding {
+                    ctx.print_color(
+                        arrow_pos.x - i,
+                        y,
+                        RGB::named(rltk::WHITE),
+                        RGB::named(rltk::GREY),
+                        &" ".to_string(),
+                    );
+                }
+                y += 1;
+            }
+            ctx.set(
+                arrow_pos.x,
+                arrow_pos.y,
+                RGB::named(rltk::WHITE),
+                RGB::named(rltk::GREY),
+                26,
+            );
+        } else {
+            let arrow_pos = Point::new(mpos.0 + 1, mpos.1);
+            let left_x = mpos.0 + 3;
+            let mut y = mpos.1;
+            for s in tooltip.iter() {
+                ctx.print_color(
+                    left_x,
+                    y,
+                    RGB::named(rltk::WHITE),
+                    RGB::named(rltk::GREY),
+                    s,
+                );
+                let padding = (width - s.len() as i32);
+                for i in 0..padding {
+                    ctx.print_color(
+                        arrow_pos.x + i,
+                        y,
+                        RGB::named(rltk::WHITE),
+                        RGB::named(rltk::GREY),
+                        &" ".to_string(),
+                    );
+                }
+                y += 1;
+            }
+            ctx.set(
+                arrow_pos.x,
+                arrow_pos.y,
+                RGB::named(rltk::WHITE),
+                RGB::named(rltk::GREY),
+                27,
+            );
         }
     }
 }
