4e23f5a31d0e0d0d3b61481b62ca79979831cdae
Matthew Drury <matthew.drury.83@gmail.com>
2021-03-31 18:13:00 -0700

    Spawn a random terrain.

--- rogue/src/terrain_spawners/mod.rs
+++ rogue/src/terrain_spawners/mod.rs
@@ -12,5 +12,15 @@ pub mod foliage;
 
 
 pub fn spawn_terrain(ecs: &mut World, map: &Map, depth: i32) {
-    foliage::spawn_grove_grass(ecs, map);
+    let roll;
+    {
+        let mut rng = ecs.write_resource::<RandomNumberGenerator>();
+        roll = rng.roll_dice(1, 3)
+    }
+    match roll {
+        1 => foliage::spawn_short_grass(ecs, map),
+        2 => foliage::spawn_sporadic_grass(ecs, map),
+        3 => foliage::spawn_grove_grass(ecs, map),
+        _ => panic!("Rolled to high on terrain spawning.")
+    }
 }
